729d8ddc87f8a3ee2ac98b820c61950d8ee7c63c
Boekel,Kelvin K. van <kelvinvanboekel@gmail.com>
2018-03-07 15:13:02 +0100

    filemanager

---
+++ Unity/PTS4Project/Assets/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileManager : MonoBehaviour {
+
+    private string path;
+    public midiSequencer ms;
+    public Dropdown dropdown;
+
+    public void onChange()
+    {
+        switch (dropdown.value)
+        {
+            case 0:
+                break;
+            case 1:
+                dropdown.value = 0;
+                OpenExplorer();
+                break;
+            case 2:
+                safeFile();
+                break;
+        }
+    }
+
+    public void OpenExplorer()
+    {
+        path = EditorUtility.OpenFilePanel("", "../", "mid");
+        if (path.Length != 0)
+        {
+            var fileContent = path;
+            Debug.Log(fileContent);
+            ms.LoadMidi(fileContent);
+        }
+    }
+
+    public void safeFile()
+    {
+        path = EditorUtility.SaveFilePanel("", "../", "", "mid");
+        if (path.Length != 0)
+        {
+        }
+    }
+}

637b40c6dae3a55f954538fd707ae12b6e4dc9aa
Unknown <michael_bbuitenweg@yahoo.com>
2018-04-04 14:29:16 +0200

    connection webservice
    connected the loginpage with the webserver, still need to implement methods

--- Unity/PTS4Project/Assets/Login.cs
+++ Unity/PTS4Project/Assets/Login.cs
@@ -1,70 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
 
-  public GameObject Username;
-  public GameObject Password;
-
-  private string username;
-  private string password;
-  private string usernamecorrect = "Test";
-  private string passwordcorrect = "Test";
-  // Use this for initialization
-  void Start()
-  {
-
-  }
-
-  public void Loginbutton()
-  {
-    if (username == "")
+    public GameObject Username;
+    public GameObject Password;
+    private string username;
+    private string password;
+    private string usernamecorrect = "Test";
+    private string passwordcorrect = "Test";
+    // Use this for initialization
+    void Start()
     {
-      print("Please fill in a Username");
+        StartCoroutine(getRequest("localhost:8080/SimpleMavenWebApp/HelloWorld"));
     }
-    else if (password == "")
-    {
-      print("Please fill in a Password");
-    }
-    else
-    {
-      Authenticate();
-    }
-  }
 
-  public void Authenticate()
-  {
-    if (username == usernamecorrect && password == passwordcorrect)
+    IEnumerator getRequest(string uri)
     {
-      print("Succesful log in. Have fun with DAW");
-    }
-    else
-    {
-      Debug.LogWarning("Sorry, try again");
-    }
-  }
-  // Update is called once per frame
-  void Update()
-  {
-    username = Username.GetComponent<InputField>().text;
-    password = Password.GetComponent<InputField>().text;
+        UnityWebRequest uwr = UnityWebRequest.Get(uri);
+        yield return uwr.SendWebRequest();
 
-    if (Input.GetKeyDown(KeyCode.Tab))
-    {
-      if (Username.GetComponent<InputField>().isFocused)
-      {
-        Password.GetComponent<InputField>().Select();
-      }
-      if (Input.GetKeyDown(KeyCode.Return))
-      {
-        if (password != "")
+        if (uwr.isNetworkError)
         {
-          Authenticate();
+            Debug.Log("Error While Sending: " + uwr.error);
         }
-      }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
     }
-  }
+
+    public void Loginbutton()
+    {
+        if (username == "")
+        {
+            print("Please fill in a Username");
+        }
+        else if (password == "")
+        {
+            print("Please fill in a Password");
+        }
+        else
+        {
+            Authenticate();
+        }
+    }
+
+    public void Authenticate()
+    {
+        if (username == usernamecorrect && password == passwordcorrect)
+        {
+            print("Succesful log in. Have fun with DAW");
+        }
+        else
+        {
+            Debug.LogWarning("Sorry, try again");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        username = Username.GetComponent<InputField>().text;
+        password = Password.GetComponent<InputField>().text;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Username.GetComponent<InputField>().isFocused)
+            {
+                Password.GetComponent<InputField>().Select();
+            }
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (password != "")
+                {
+                    Authenticate();
+                }
+            }
+        }
+    }
 }

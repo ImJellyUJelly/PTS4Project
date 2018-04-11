dda3cb81b50b78daa1d0e64e4879491791d3221d
Gijs <g.vandenbiggelaar@gmail.com>
2018-04-11 11:00:17 +0200

    Further implemented Dialog Box

--- Unity/PTS4Project/Assets/DialogBox.cs
+++ Unity/PTS4Project/Assets/DialogBox.cs
@@ -1,9 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogBox : MonoBehaviour {
 
+    public string message;
+    public ButtonType type;
 
+    [SerializeField]
+    private GameObject YesNoGroup;
+    [SerializeField]
+    private GameObject OKGroup;
 
+    private void Start()
+    {
+        message = message.Replace(';', '\n');
+        GetComponentInChildren<Text>().text = message;
+
+        if(type == ButtonType.OK)
+        {
+            OKGroup.SetActive(true);
+            YesNoGroup.SetActive(false);
+        }
+        else
+        {
+            OKGroup.SetActive(false);
+            YesNoGroup.SetActive(true);
+        }
+
+        GetComponent<FadePanel>().visible = true;
+    }
+
+}
+
+public enum ButtonType
+{
+    YesNo,
+    OK
 }

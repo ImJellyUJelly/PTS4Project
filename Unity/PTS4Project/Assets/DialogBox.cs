de0aab2bee9f8cbe3e3bfbde08eb273b1627c04d
Gijs <g.vandenbiggelaar@gmail.com>
2018-04-11 11:09:56 +0200

    Press P to show a funny message box

--- Unity/PTS4Project/Assets/DialogBox.cs
+++ Unity/PTS4Project/Assets/DialogBox.cs
@@ -5,20 +5,27 @@ using UnityEngine.UI;
 
 public class DialogBox : MonoBehaviour {
 
-    public string message;
-    public ButtonType type;
+    private string message;
 
     [SerializeField]
     private GameObject YesNoGroup;
     [SerializeField]
     private GameObject OKGroup;
 
-    private void Start()
+    private void Update()
     {
-        message = message.Replace(';', '\n');
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Show("you're mom triple gay;XD", ButtonType.OK);
+        }
+    }
+
+    public void Show(string msg, ButtonType type)
+    {
+        message = msg.Replace(';', '\n');
         GetComponentInChildren<Text>().text = message;
 
-        if(type == ButtonType.OK)
+        if (type == ButtonType.OK)
         {
             OKGroup.SetActive(true);
             YesNoGroup.SetActive(false);
@@ -30,6 +37,13 @@ public class DialogBox : MonoBehaviour {
         }
 
         GetComponent<FadePanel>().visible = true;
+    }
+
+    public void DismissOK()
+    {
+        GetComponent<FadePanel>().visible = false;
+        OKGroup.SetActive(false);
+        YesNoGroup.SetActive(false);
     }
 
 }

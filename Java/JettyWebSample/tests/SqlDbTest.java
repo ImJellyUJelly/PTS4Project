90d22c739b5e6ccea7ea835cd7c0ac0e22c3b17d
Jelle Schräder <jelleschrader@gmail.com>
2018-04-18 14:44:54 +0200

    empty dbtests added

---
+++ Java/JettyWebSample/tests/SqlDbTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class SqlDbTest {
+
+    @org.junit.Test
+    public void getMidiStringById() {
+        // TODO: Create test
+    }
+
+    @org.junit.Test
+    public void insertMidiString() {
+        // TODO: Create test
+    }
+}

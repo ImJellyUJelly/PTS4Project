5eb41872998b37e9c9d299d02fc6005311c9744d
Jelle Schräder <jelleschrader@gmail.com>
2018-04-18 13:50:06 +0200

    nog eens commit, omdat we blijkbaar van Branch veranderd waren

--- Java/JettyWebSample/src/main/database/SqlDb.java
+++ Java/JettyWebSample/src/main/database/SqlDb.java
@@ -1,11 +1,53 @@
 package main.database;
 
+import java.sql.PreparedStatement;
+
 public class SqlDb {
     // This class will be used to push statements to the database, using Database-class for: connection, statement and resultset.
-    private Database database;
-    public SqlDb() {
-        this.database = new Database();
+
+    public String getMidiStringById(int id) {
+        Database sqlConnection = new Database();
+        try{
+            sqlConnection.setStatement(sqlConnection.getConnection().createStatement());
+            String query = ""; // TODO: Make the query
+
+            PreparedStatement ps = sqlConnection.getConnection().prepareStatement(query);
+            ps.setInt(1, id);
+
+            sqlConnection.setResult(ps.executeQuery());
+
+            String result = null;
+            while(sqlConnection.getResult().next()) {
+                result = sqlConnection.getResult().getString(1); // TODO: Find the right row to return from the Id.
+            }
+            return result;
+        } catch(Exception ex) {
+            //Do nothing
+            ex.printStackTrace();
+        }
+        finally{
+            sqlConnection.closeAll();
+        }
+        return null;
     }
 
+    public void insertMidiString(String midiFile) {
+        Database sqlConnection = new Database();
+        try{
+            sqlConnection.setStatement(sqlConnection.getConnection().createStatement());
+            String query = ""; // TODO: Make the query
 
+            PreparedStatement ps = sqlConnection.getConnection().prepareStatement(query);
+
+            sqlConnection.setResult(ps.executeQuery());
+
+            // TODO: input the String into the database
+        } catch(Exception ex) {
+            //Do nothing
+            ex.printStackTrace();
+        }
+        finally{
+            sqlConnection.closeAll();
+        }
+    }
 }

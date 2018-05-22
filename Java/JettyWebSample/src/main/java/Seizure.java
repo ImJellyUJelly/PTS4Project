a96e46ca16b0d710da9b219f1b7fb82bd4e35062
Jelle Schräder <jelleschrader@gmail.com>
2018-05-22 10:05:30 +0200

    DAL tests added

--- Java/JettyWebSample/src/main/java/Seizure.java
+++ Java/JettyWebSample/src/main/java/Seizure.java
@@ -1,6 +1,20 @@
 package main.java;
 
+import main.database.SqlDb;
+
 public class Seizure { // facade
     // Nou succes jongens, bring me code
-    
+    private SqlDb sqlDb;
+
+    public Seizure() {
+        sqlDb = new SqlDb();
+    }
+
+    public String loadMidiString(int id) {
+        return sqlDb.getMidiStringById(id);
+    }
+
+    public void saveMidiString(String midi) {
+        sqlDb.insertMidiString(midi);
+    }
 }

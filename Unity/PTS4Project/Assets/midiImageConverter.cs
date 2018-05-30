16ed0ae1db5796ca2778336958a87e55a0cf8fbf
Riet,Redmar R.F. van de <r.vanderiet@student.fontys.nl>
2018-05-30 13:34:55 +0200

    added midi 2 sheet png version

---
+++ Unity/PTS4Project/Assets/midiImageConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class midiImageConverter : MonoBehaviour {
+
+    private const string converterPath = "sheet.exe";
+    private ProcessStartInfo pInfo;
+    
+
+	// Use this for initialization
+	void Start () {
+        pInfo =  new ProcessStartInfo();
+        pInfo.CreateNoWindow = false;
+        pInfo.UseShellExecute = false;
+        pInfo.FileName = converterPath;
+        pInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        pInfo.Arguments = "";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
+        {
+            UnityEngine.Debug.Log("Trying to print midi sheet");
+
+            try
+            {
+                using (Process exe = Process.Start(pInfo))
+                {
+                    pInfo.Arguments = "akachan.mid" + " " + "test";
+                    exe.WaitForExit();
+                }
+            }
+            catch (System.Exception e)
+            {
+                print("Error occured in midiImageConvert::Update\n");
+                print(e);
+            }
+        }
+	}
+
+
+}
